5b52a6846a9a994f1658e181d160b8fa5358e371
mariya.kroxaleva <mariya.kroxaleva@maginfo.com>
2015-05-15 09:41:54 +0500

    use BigdataGraphClient.loadGraphML() instead GraphMLReader.inputGraph()

--- sample-blueprints-remote/src/main/java/sample/blueprints/remote/SampleBlazegraphBlueprintsRemote.java
+++ sample-blueprints-remote/src/main/java/sample/blueprints/remote/SampleBlazegraphBlueprintsRemote.java
@@ -1,26 +1,23 @@
 package sample.blueprints.remote;
 
-import java.io.IOException;
-
 import org.apache.log4j.Logger;
 
 import com.bigdata.blueprints.BigdataGraph;
 import com.bigdata.blueprints.BigdataGraphClient;
 import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
-import com.tinkerpop.blueprints.util.io.graphml.GraphMLReader;
 
 
 public class SampleBlazegraphBlueprintsRemote {
 	
 	protected static final Logger log = Logger.getLogger(SampleBlazegraphBlueprintsRemote.class);
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		
 		final BigdataGraph graph = new BigdataGraphClient("http://localhost:9999/bigdata");
-               
-        GraphMLReader.inputGraph(graph, SampleBlazegraphBlueprintsRemote.class.getResourceAsStream("/graph-example-1.xml"));
-        
+		
+			graph.loadGraphML(SampleBlazegraphBlueprintsRemote.class.getResource("/graph-example-1.xml").getFile());
+
 		for (Vertex v : graph.getVertices()) {
 			log.info(v);
 		}
