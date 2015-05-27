80da4d6fe40b0b4e7a1774b10d87e465bf7dd269
mariya.kroxaleva <mariya.kroxaleva@maginfo.com>
2015-05-27 10:23:26 +0500

    add license, wiki links

--- sample-sesame-first/src/main/java/HelloBlazegraph.java
+++ sample-sesame-first/src/main/java/HelloBlazegraph.java
@@ -1,3 +1,32 @@
+/**
+
+Copyright (C) SYSTAP, LLC 2006-2015.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     2501 Calvert ST NW #106
+     Washington, DC 20008
+     licenses@systap.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+ * Wiki page link:
+ * http://wiki.blazegraph.com/wiki/index.php/First_Application_Tutorial
+ */
+
 import java.util.Properties;
 
 import org.openrdf.OpenRDFException;
@@ -13,17 +42,16 @@ import org.openrdf.query.TupleQueryResult;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 
+import com.bigdata.journal.BufferMode;
 import com.bigdata.journal.Options;
 import com.bigdata.rdf.sail.BigdataSail;
 import com.bigdata.rdf.sail.BigdataSailRepository;
-
-
 
 public class HelloBlazegraph {
 	public static void main(String[] args) throws OpenRDFException {
 
 		final Properties props = new Properties();
-		props.put(Options.BUFFER_MODE, "DiskRW"); // persistent file system located journal
+		props.put(Options.BUFFER_MODE, BufferMode.DiskRW); // persistent file system located journal
 		props.put(Options.FILE, "/tmp/blazegraph/test.jnl"); // journal file location
 
 		final BigdataSail sail = new BigdataSail(props); // instantiate a sail
--- sample-sesame-remote/src/main/java/sample/sesame/remote/SampleBlazegraphSesameRemote.java
+++ sample-sesame-remote/src/main/java/sample/sesame/remote/SampleBlazegraphSesameRemote.java
@@ -1,9 +1,37 @@
+/**
+
+Copyright (C) SYSTAP, LLC 2006-2015.  All rights reserved.
+
+Contact:
+     SYSTAP, LLC
+     2501 Calvert ST NW #106
+     Washington, DC 20008
+     licenses@systap.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+ * Wiki page link:
+ * http://wiki.blazegraph.com/wiki/index.php/Sesame_API_remote_mode
+ * 
+ */
+
 package sample.sesame.remote;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
